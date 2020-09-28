7c23fa8d9efde58e310e3c27f8ba14666c745cd6
Sage Livingstone <sagewlivingstone@gmail.com>
2020-09-27 23:03:47 -0500

    partial parsing algorithm completion

--- service.py
+++ service.py
@@ -13,6 +13,9 @@ ENV_CV_ENDPOINT = 'COMPUTER_VISION_ENDPOINT'
 
 TEST_SCOREBOARD_IMAGE = 'https://i.stack.imgur.com/i5jSA.jpg'
 
+SIEGE_NUMERAL_EXP = r"^[Oo\d]{0,4}$"
+SIEGE_NAME_EXP = r"^(?=.{3,15}$)(?![_.0-9-])[a-zA-Z0-9._-]+$"
+
 
 def hist(a: float, r: float) -> float:
     """
@@ -57,7 +60,7 @@ class BoundingBox():
 
 class TextItem():
     """
-    model for text item found from read result
+    model for text item found from image read result
     """
 
     def __init__(self, line: Line):
@@ -80,6 +83,26 @@ class TextItem():
             )
         )
 
+        self._get_text_type()
+
+    def _get_text_type(self):
+        numeral = re.match(SIEGE_NUMERAL_EXP, self.text)
+        name = re.match(SIEGE_NAME_EXP, self.text)
+
+        if numeral and name:
+            # Both??? ... do sanity check later
+            self.type = 'mix'
+            self.anchor = self.left_anchor
+        elif numeral:
+            self.type = 'numeral'
+            self.anchor = self.center_anchor
+        elif name:
+            self.type = 'name'
+            self.anchor = self.left_anchor
+        else:
+            self.type = 'neither'
+            self.anchor = self.left_anchor
+
     @staticmethod
     def _parse_bounding_box_arr(bounding_box: list) -> BoundingBox:
         parsed = list()
@@ -94,7 +117,6 @@ class TextItem():
         return BoundingBox(parsed)
 
 
-# TODO: Create a Scoreboard class that holds one of these:
 class ImageResult():
     """
     object that hold methods for parsed image result from Azure OCR API
@@ -109,54 +131,6 @@ class ImageResult():
                     # Add entry for text item
                     self.text_items.append(TextItem(line))
 
-    @staticmethod
-    def get_siege_text_anchor(text_item: TextItem):
-        """
-        return type of anchor for this text item based on text from a siege
-        scoreboard - updated 9/23/20
-        -------
-        we know:
-        numerals only - center anchor
-        contains alphanumerals - left anchor
-        """
-        numeral_exp = r"^\d{0,4}$"
-        name_exp = r"^(?=.{3,15}$)(?![_.0-9-])[a-zA-Z0-9._-]+$"
-
-        numeral = re.match(numeral_exp, text_item.text)
-        name = re.match(name_exp, text_item.text)
-
-        if numeral and name:
-            # TODO: both matched... do a sanity check?
-            return text_item.left_anchor
-        if numeral:
-            return text_item.center_anchor
-        if name:
-            return text_item.left_anchor
-        return None
-
-    def get_text_hist_dict(self) -> dict:
-        """
-        get the histogram dictionary of x, y coordinates for text results
-        """
-        # TODO: This probably needs to be moved into a scoreboard class
-        res = dict()
-        for item in self.text_items:
-            # Figure out of it is a left anchored or center-anchored text
-            anchor = ImageResult.get_siege_text_anchor(item)\
-                     or item.center_anchor
-
-            x = hist(anchor.x, 20)
-            y = hist(anchor.y, 20)
-
-            if x not in res:
-                res[x] = dict()
-            if y not in res[x]:
-                res[x][y] = list()
-
-            res[x][y].append(item)
-
-        return res
-
     def print_read_info(self):
         for item in self.text_items:
             print("Item Text:", "'" + item.text + "'")
@@ -168,6 +142,63 @@ class ImageResult():
                   item.left_anchor.y)
             print()
 
+
+class Parser():
+    def __init__(self,
+                 client: ComputerVisionClient = None,
+                 url: str = None):
+        self._client = client
+        self._url = url
+        if client and url:
+            self.parse_image(client, url)
+
+    def parse_image(self, client, url):
+        self._image_text = self.read_image_text(client, url)
+        self._result = ImageResult(self._image_text)
+
+    def read_image_text(self, client: ComputerVisionClient, remote_url: str):
+        """
+        read text from an image using the Azure OCR Read API
+        """
+        print(f"Calling read API on {remote_url}")
+
+        read_result = client.read(remote_url, raw=True)
+
+        operation_location_remote = read_result.headers['Operation-Location']
+        operation_id = operation_location_remote.split('/')[-1]
+
+        # GET method for read results
+        while True:
+            read_operation_result = client.get_read_result(operation_id)
+            if read_operation_result.status not in [
+                OperationStatusCodes.not_started,
+                    OperationStatusCodes.running]:
+                break
+            time.sleep(1)  # Re-check every second
+
+        return read_operation_result if read_operation_result.status ==\
+            OperationStatusCodes.succeeded else None
+
+    def get_text_hist_dict(self) -> dict:
+        """
+        get the histogram dictionary of x, y coordinates for text results
+        """
+        # TODO: This probably needs to be moved into a scoreboard class
+        res = dict()
+        for item in self._result.text_items:
+            # Figure out of it is a left anchored or center-anchored text
+            x = hist(item.anchor.x, 60)
+            y = hist(item.anchor.y, 30)
+
+            if x not in res:
+                res[x] = dict()
+            if y not in res[x]:
+                res[x][y] = list()
+
+            res[x][y].append(item)
+
+        return res
+
     def print_hist_dict(self):
         text_items = self.get_text_hist_dict()
 
@@ -177,6 +208,92 @@ class ImageResult():
                 print("  y:", y)
                 for text_item in row_items:
                     print("    ", text_item.text)
+
+    def parse_siege_scoreboard(self):
+        # Algorithm overview:
+        hist = self.get_text_hist_dict()
+
+        # 1. find all the usernames, validate that there's at least 6~7
+        stat_cols = {}
+        names = {}
+        for x, ys in hist.items():
+            if len(ys) > 6:
+                # This could be considered a 'column', check for names
+                name_count = 0
+                numeral_count = 0
+                neither_count = 0
+                for y, items in ys.items():
+                    # Check if this is a name
+                    for item in items:
+                        if item.type == 'name':
+                            name_count += 1
+                        if item.type == 'numeral':
+                            numeral_count += 1
+                        if item.type == 'neither':
+                            neither_count += 1
+                
+                # Now, determine what kind of column this is
+                if name_count > 6:
+                    # This is the name column
+                    names = ys
+                    continue
+                elif numeral_count > 6:
+                    stat_cols[x] = ys
+                    continue
+
+                # maybe debug 'neither' and 'both' later
+
+        print(f"Found {len(stat_cols)} stat columns")
+        if len(stat_cols) == 5:
+            # Ok we got 5 stat columns, so far so good
+            # now figure out which column is which
+            keys = sorted(stat_cols.keys())
+            points_col = stat_cols[keys[0]]
+            kills_col = stat_cols[keys[1]]
+            assists_col = stat_cols[keys[2]]
+            deaths_col = stat_cols[keys[3]]
+            ping_col = stat_cols[keys[4]]
+        
+        for y, player in names.items():
+            if len(player) == 1:
+                player = player[0].text
+            else:
+                return  # this is a problem...
+            
+            try:
+                for score in points_col[y]:
+                    if score.type == 'numeral':
+                        points = score.text
+                        break
+            except KeyError:
+                print(f"Could not find points for {player} at {y}")
+                for cy, scores in points_col.items():
+                    for score in scores:
+                        print(f"  {cy} = {score.text}")
+                continue
+            try:
+                for kill in kills_col[y]:
+                    if kill.type == 'numeral':
+                        kills = kill.text
+                        break
+            except KeyError:
+                print(f"Could not find kills for {player} at {y}")
+                continue
+            print(f"Player {player} :  {points} | {kills}")
+
+        self.print_hist_dict()
+            
+        # for x, col in stat_cols.items():
+        #     print("Found column:")
+        #     for y, items in col.items():
+        #         for item in items:
+        #             print(f"    : {item.text}")
+
+        # 2. find each stat column with same # of entries as players, save as a guess
+        # 3. for each stat, try to find the player that matches it and save to sb object
+
+        # 4. find score to the left of player names
+        # 5. find map name right above mode
 
 
 def _authenticate() -> ComputerVisionClient:
@@ -234,38 +351,18 @@ def get_image_category(client: ComputerVisionClient, remote_url: str):
             print(f"{category.name} with confidence {category.score * 100}")
 
 
-def read_image_text(client: ComputerVisionClient, remote_url: str):
-    """
-    read text from an image using the Azure OCR Read API
-    """
-    print(f"Calling read API on {remote_url}")
-
-    read_result = client.read(remote_url, raw=True)
-
-    operation_location_remote = read_result.headers['Operation-Location']
-    operation_id = operation_location_remote.split('/')[-1]
-
-    # GET method for read results
-    while True:
-        read_operation_result = client.get_read_result(operation_id)
-        if read_operation_result.status not in [
-            OperationStatusCodes.not_started,
-                OperationStatusCodes.running]:
-            break
-        time.sleep(1)  # Re-check every second
-
-    return read_operation_result if read_operation_result.status ==\
-        OperationStatusCodes.succeeded else None
-
-
 def main():
     client = _authenticate()
 
     # Call OCR API to get text readings and bounds
-    image_text = read_image_text(client, TEST_SCOREBOARD_IMAGE)
+    p = Parser(client, TEST_SCOREBOARD_IMAGE)
+    p.parse_siege_scoreboard()
+    # p._result.print_hist_dict()
+    # image_text = read_image_text(client, TEST_SCOREBOARD_IMAGE)
     # Parse image results and build dict of entries
-    ir = ImageResult(image_text)
-    ir.print_hist_dict()
+    # ir = ImageResult(image_text)
+    # ir.print_hist_dict()
+
     # ir.print_read_info()
 
 
